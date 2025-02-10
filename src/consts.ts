a6fd25a9e48f76b82c9937987a294499bda114d4
leeworms <ljg960730@naver.com>
2025-02-10 22:51:02 +0900

    mod: default description 수정

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,8 @@
 export const SITE = {
   title: 'leeworms',
   titleTemplate: '%s',
-  description: 'Develop about something Soft and Simple.',
-  image:
-    'https://avatars.githubusercontent.com/u/64590527?v=4',
+  description: '안녕하세요. 백앤드 개발자 이재근 입니다. 꾸준한 성장을 위한 블로그 공간을 운영중입니다. 개발 경험과 지식을 공유하고자 합니다.',
+  image: 'https://avatars.githubusercontent.com/u/64590527?v=4',
 } as const;
 
 export const NAV = ['writing', 'note'] as const;
